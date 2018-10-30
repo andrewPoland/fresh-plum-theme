a90be973164582a9d8834fa5fe8827806c3e966b
Andrew Poland <ajp159@live.com>
2018-10-30 13:47:34 -0700

    fixed up plum theme branch and got bootstrap working with webpack. started initial work on templating.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module:{
+    rules:[
+            {
+              test: /\.(scss)$/,
+              use: [{
+                loader: 'style-loader', // inject CSS to page
+              }, {
+                loader: 'css-loader', // translates CSS into CommonJS modules
+              }, {
+                loader: 'postcss-loader', // Run post css actions
+                options: {
+                  plugins: function () { // post css plugins, can be exported to postcss.config.js
+                    return [
+                      require('precss'),
+                      require('autoprefixer')
+                    ];
+                  }
+                }
+              }, {
+                loader: 'sass-loader' // compiles Sass to CSS
+              }]
+             }
+     ]
+  },
+  node: { 
+    fs: 'empty'
+  }
+};
