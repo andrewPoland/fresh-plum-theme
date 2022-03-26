af44d87ea193dbefe521a179cf40634e9356b1da
Andrew Poland <andrewpo.software@gmail.com>
2022-03-26 13:42:56 +1300

    minor updates to theming and styling to support better pdf printing. Updated package libraries where possible making some changes for support font awesome changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 import 'bootstrap'
 import './../scss/plum-theme.scss';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 function component() {
     var element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -6,41 +6,45 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  module:{
-    rules:[
-            {
-              test: /\.(scss)$/,
-              use: [{
-                loader: 'style-loader', // inject CSS to page
-              }, {
-                loader: 'css-loader', // translates CSS into CommonJS modules
-              }, {
-                loader: 'postcss-loader', // Run post css actions
-                options: {
-                  plugins: function () { // post css plugins, can be exported to postcss.config.js
-                    return [
-                      require('precss'),
-                      require('autoprefixer')
-                    ];
-                  }
-                }
-              }, {
-                loader: 'sass-loader' // compiles Sass to CSS
-              }]
-             },
-             {
-              test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-              use: [{
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: '../fonts'
-                }
-              }]
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            postcssOptions: {
+              plugins: function () { // post css plugins, can be exported to postcss.config.js
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
             }
-     ]
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: '../fonts'
+          }
+        }]
+      }
+    ]
   },
-  node: { 
-    fs: 'empty'
+  resolve: {
+    fallback: {
+      fs: 'empty'
+    }
   }
 };
